ce8ebb8843e18c10f911af0b9fa5f2efa544bf52
Fanny CARLIER <75118010+Peterpaine01@users.noreply.github.com>
2025-04-28 02:00:27 +0200

    authorize router

--- src/Router/allRoutes.js
+++ src/Router/allRoutes.js
@@ -2,29 +2,37 @@ import Route from "./Route.js"
 
 //Define Routes here
 export const allRoutes = [
-  new Route("/", "Accueil", "/pages/home.html"),
-  new Route("/galerie", "Galerie", "/pages/galery.html", "/js/galery.js"),
+  new Route("/", "Accueil", "/pages/home.html", []),
+  new Route("/galerie", "Galerie", "/pages/galery.html", [], "/js/galery.js"),
   new Route(
     "/connexion",
     "Connexion",
     "/pages/auth/login.html",
+    ["disconnected"],
     "/js/auth/signin.js"
   ),
   new Route(
     "/inscription",
     "Inscription",
     "/pages/auth/signup.html",
+    ["disconnected"],
     "/js/auth/signup.js"
   ),
-  new Route("/compte", "Mon Compte", "/pages/auth/account.html"),
+  new Route("/compte", "Mon Compte", "/pages/auth/account.html", [
+    "client",
+    "admin",
+  ]),
   new Route(
-    "/modifier-mot-de-passe",
-    "Modifier mot de passe",
-    "/pages/auth/editPassword.html"
+    "/editPassword",
+    "Changer de mot de passe",
+    "/pages/auth/editPassword.html",
+    ["client", "admin"]
   ),
-  new Route("/carte", "La Carte", "/pages/menu.html"),
-  new Route("/reservations", "Vos Réservations", "/pages/bookingsList.html"),
-  new Route("/reserver", "Réserver", "/pages/bookingAdd.html"),
+  new Route("/carte", "La Carte", "/pages/menu.html", []),
+  new Route("/reservations", "Vos Réservations", "/pages/bookingsList.html", [
+    "client",
+  ]),
+  new Route("/reserver", "Réserver", "/pages/bookingAdd.html", ["client"]),
 ]
 
 //Title is displayed such as : Route.title - websitename
